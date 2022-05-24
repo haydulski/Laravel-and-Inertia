7d26ff308165748a81eebd44d3cced2c845d9312
haydulski <damian.hajdukiewicz@gmail.com>
2022-05-24 20:39:57 +0200

    First commit

---
+++ resources/js/components/Pages/Damian.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Damian({ event }) {
+    return (<>
+        <h1>
+            Test page with passed data: {event}
+        </h1>
+    </>);
+}
+
+export default Damian;
